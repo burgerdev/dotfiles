0d89fab93c796bd682ae0815182a38177b6ce078
Markus Rudy <markus.rudy@sap.com>
2017-10-02 13:30:01 +0200

    only extend path if dir present

--- config/fish/functions/adjust_linker_path.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-
-function adjust_linker_path
-    set -x -g LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/lib
-    set -x -g LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/lib
-end
--- config/fish/functions/adjust_path.fish
+++ config/fish/functions/adjust_path.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/fish
 function adjust_path
-    set -x -g PATH $PATH $HOME/.local/bin
+    set localpath "$HOME/.local/bin"
+    if test -d "$localpath"
+        set -x -g PATH $PATH "$localpath"
+    end
 end
 
