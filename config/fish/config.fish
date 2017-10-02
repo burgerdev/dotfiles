f3d20248fdb7bae78ad7c541ec526f1509696882
Markus Rudy <markus.rudy@sap.com>
2017-10-02 20:22:20 +0200

    special prompt for root

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/fish
 
 adjust_path
-adjust_linker_path
 adjust_env
 
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,9 +2,20 @@ function fish_prompt
 
 set OLD_STATUS $status
 
-echo -n (set_color -o $fish_color_host)
-echo -n $USER
-echo -n "@"
+switch (uname)
+    case Darwin
+            echo -n (set_color normal)
+    case '*'
+            echo -n (set_color -o)
+end
+
+if test (whoami) = root 
+    echo -n (set_color red)
+else
+    echo -n (set_color $fish_color_host)
+    echo -n $USER
+    echo -n "@"
+end
 echo -n (fish_prompt_hostname)
 echo -n " "
 echo -n (fish_prompt_pwd)
@@ -13,6 +24,10 @@ echo -n (fish_prompt_git)
 echo ""
 echo -n (fish_prompt_status $OLD_STATUS)
 echo -n (set_color normal)
+if test (whoami) = root 
+echo -n "# "
+else
 echo -n "> "
+end
 
 end
