a03c333a493e1ed6c8655643a519c31cad6ff071
Markus Rudy <webmaster@burgerdev.de>
2019-01-10 21:41:09 +0100

    fix xmobar dock for xmonad 0.15

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -10,29 +10,17 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-moveAppsToWorkspaces = composeAll
-    [ className =? "Firefox" --> doShift "2:web"
-    , className =? "Thunderbird" --> doShift "2:web"
-    , className =? "chromium" --> doShift "2:web"
-    , className =? "Unreal Editor" --> doShift "5"
-    , title =? "Unreal" --> doShift "5"
---    , title =? "Figure 1" --> doShift "4:app"
-    ]
--- get className via xprop
-
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/burger/.xmobarrc"
-    xmonad $ ewmh defaultConfig
-        { workspaces = ["1:dev", "2:web", "3:script", "4:code", "5", "6:ssh", "7", "8", "9:root"]
-        , manageHook = moveAppsToWorkspaces <+> manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
-        , layoutHook = smartBorders . avoidStruts $ onWorkspace "2:web" Full $ layoutHook defaultConfig
+    xmonad =<< xmobar defaultConfig
+        { workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = smartBorders . avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
---        , modMask = mod4Mask
---        , terminal = "konsole"
-        , terminal = "lilyterm"
+        , terminal = "kitty"
 -- for Java Swing apps
         , startupHook = setWMName "LG3D"
         }
