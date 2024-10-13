288186d61b4a5fa444056be655c956313e885ad7
Markus Rudy <webmaster@burgerdev.de>
2024-10-13 18:03:29 +0200

    move ~/.local/bin to beginning of PATH

--- config/fish/functions/adjust_path.fish
+++ config/fish/functions/adjust_path.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/fish
 function adjust_path
-    set localpath "$HOME/.local/bin"
-    if test -d "$localpath"
-        set -x -g PATH $PATH "$localpath"
-    end
-
     if which opam 2>/dev/null >&2
         eval (opam config env)
+    end
+
+    set localpath "$HOME/.local/bin"
+    if test -d "$localpath"
+        set -x -g PATH "$localpath" $PATH
     end
 end
 
